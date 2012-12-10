c4d5517e5ff30866044352a53f032246a8928746
Lakmal Warusawithana <lakmal@wso2.com>
2012-12-10 15:15:27 +0530

    fixed db user

--- www/config.php
+++ www/config.php
@@ -51,10 +51,10 @@ $sugar_config = array (
   'datef' => 'm/d/Y',
   'dbconfig' => 
   array (
-    'db_host_name' => getenv(STRATOS_DB_HOST),
+    'db_host_name' => getenv(STRATOS_MYSQL_HOST),
     'db_host_instance' => 'SQLEXPRESS',
-    'db_user_name' => getenv(STRATOS_DB_USER),
-    'db_password' => getenv(STRATOS_DB_PASSWORD),
+    'db_user_name' => getenv(STRATOS_MYSQL_USER),
+    'db_password' => getenv(STRATOS_MYSQL_PASSWORD),
     'db_name' => 'sugarcrm',
     'db_type' => 'mysql',
     'db_port' => '',
